79faf0d73e31aba039d370a239f70601f9b05ac1
artesvisuais <arte.fx.domingues@gmail.com>
2026-04-13 21:48:38 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  URL_SCRIPT: "https://script.google.com/macros/s/AKfycbz4gGZHU6qtpFXMCQQn0b8yjtuK4ZTZVG45mchiB7gn8XGdIIoxF1sQKANxEBEbdTg/exec",
+  VERSAO: "1.0.3",
+  NOME_APP: "Chamada Digital",
+  MODO_DEBUG: false
+};
